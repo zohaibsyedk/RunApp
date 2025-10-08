66ca3c8ba4f36c078700c0e4e16ade1034cacd9e
nickmanna <nickmanna3@gmail.com>
2025-10-07 20:59:52 -0500

    Update Tabs

--- run-app/app/(tabs)/_layout.tsx
+++ run-app/app/(tabs)/_layout.tsx
@@ -1,14 +1,39 @@
 import React from "react";
+import { View } from 'react-native';
 import { router, Tabs } from "expo-router";
 import { Ionicons } from '@expo/vector-icons';
 
 const TabLayout: React.FC = () => {
     return (
-        <Tabs>
-            <Tabs.Screen name="feed" options={{ title: 'Feed' }} />
-            <Tabs.Screen name="activity" options={{ title: 'Activity' }} />
+        <Tabs screenOptions={{
+            tabBarActiveTintColor: 'rgba(102, 222, 255)',
+            tabBarInactiveTintColor: 'rgba(191, 191, 191)',
+            tabBarStyle: {
+                backgroundColor: '#f0f0f0',
+            },
+            tabBarShowLabel: false,
+            tabBarItemStyle: {
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '150%'
+            },
+            tabBarIconStyle: {
+                height: '100%',
+                width: '100%',
+            }
             
-            <Tabs.Screen name="events" options={{ title: 'Events' }}
+        }} >
+            <Tabs.Screen name="feed" options={{ 
+                tabBarIcon: ({ color }) => <Ionicons name='home' size={40} color={color} /> 
+            }} />
+            <Tabs.Screen name="activity" options={{ 
+                tabBarIcon: ({ color }) => <Ionicons name='pulse' size={40} color={color} /> 
+            }} />
+            
+            <Tabs.Screen name="events" options={{ 
+                tabBarIcon: ({ color }) => <Ionicons name='add-circle' size={40} color={color} /> 
+            }}
             listeners={{
                 tabPress: (e) => {
                     e.preventDefault();
@@ -17,8 +42,12 @@ const TabLayout: React.FC = () => {
             }}
              />
 
-             <Tabs.Screen name="friends" options={{ title: "Friends"}} />
-            <Tabs.Screen name="profile" options={{ title: 'Profile' }} />
+            <Tabs.Screen name="friends" options={{ 
+                tabBarIcon: ({ color }) => <Ionicons name='people' size={40} color={color} /> 
+            }} />
+            <Tabs.Screen name="profile" options={{ 
+                tabBarIcon: ({ color }) => <Ionicons name='person-circle' size={40} color={color} /> 
+            }} />
         </Tabs>
       );
 }
